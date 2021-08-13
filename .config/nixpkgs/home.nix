009e6a4e8d340a3407b6b9a2635b4965af4b52a0
James Leslie <jamesleslie@posteo.net>
2021-08-13 19:07:20 -0400

    General update.

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+
+  imports =
+    [ # Include the results of the hardware scan.
+      /home/james/Documents/Nix/passbemenu
+    ];
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
@@ -19,9 +24,10 @@
   # changes in each release.
   home.stateVersion = "21.05";
 
-  home.packages = let unstable = import (builtins.fetchTarball "https://releases.nixos.org/nixpkgs/nixpkgs-21.11pre307598.5f746317f10/nixexprs.tar.xz") {}; in
+  home.packages = let unstable = import (builtins.fetchTarball "https://releases.nixos.org/nixpkgs/nixpkgs-21.11pre307598.5f746317f10/nixexprs.tar.xz") {};
+                  in
                   with pkgs; [
                     unstable.haskellPackages.Agda
-                    unstable.mu
                   ];
+
 }
