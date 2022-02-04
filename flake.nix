df1918cdebeebc6f15ea305d0ee8fca61e53ec4a
James Leslie <jamesleslie@posteo.net>
2022-02-04 15:45:58 +0000

    [general] General updates

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
   description = "James' NixOS system configuration Flake";
 
+  nixConfig.extra-substituters = "https://nix-community.cachix.org https://jeslie0.cachix.org";
+  nixConfig.extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= jeslie0.cachix.org-1:orKPykG+p5gEbLe/ETPIQdAbK5WtUl2C6CZ+aVn0dy8=";
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
@@ -18,10 +21,9 @@
     swayBGChangerGitHub.inputs.nixpkgs.follows = "nixpkgs";
 
     # This is a bleeding edge version of emacs. Sometimes, it doesn't get pulled from Cachix...
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    # emacs-overlay.url = "github:nix-community/emacs-overlay";
     # This is a nice working version of emacs \/.
-    # emacs-overlay.url = "github:nix-community/emacs-overlay/c77eefc7683c6c56694e4516f6bd5fc6b3b0cf48";
-    # emacs-overlay.url = "github:nix-community/emacs-overlay/011ec0706fa17de340c96d7d393c00af41f65cab"
+    emacs-overlay.url = "github:nix-community/emacs-overlay/9b87f32ac912eeadc444f3920d41105ab697a123";
     emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
     nur.url = "github:nix-community/NUR";
@@ -29,39 +31,39 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
-  let
-    system = "x86_64-linux"; #current system
-    pkgs = import nixpkgs {
-      inherit system;
-      config = { allowUnfree = true; };
-    };
+    let
+      system = "x86_64-linux"; #current system
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
 
-    lib = nixpkgs.lib;
+      lib = nixpkgs.lib;
 
-  in {
-    homeManagerConfigurations = {
-      james = home-manager.lib.homeManagerConfiguration {
-        inherit system pkgs;
-        username = "james";
-        homeDirectory = "/home/james";
-        configuration = {
-          imports = [
-            ./.config/NixSystem/home.nix
+    in {
+      homeManagerConfigurations = {
+        james = home-manager.lib.homeManagerConfiguration {
+          inherit system pkgs;
+          username = "james";
+          homeDirectory = "/home/james";
+          configuration = {
+            imports = [
+              ./.config/NixSystem/home.nix
+            ];
+          };
+        };
+      };
+
+      nixosConfigurations = {
+        James-Nix = lib.nixosSystem {
+          inherit system;
+
+          modules = [
+            (import ./.config/NixSystem/configuration.nix { inherit self; })
           ];
         };
       };
+
+
     };
-
-    nixosConfigurations = {
-      James-Nix = lib.nixosSystem {
-        inherit system;
-
-        modules = [
-          (import ./.config/NixSystem/configuration.nix { inherit self; })
-        ];
-      };
-    };
-
-
-  };
 }
