f78e6bfe1254c87c385ebca52d24e15be98725ea
James Leslie <jamesleslie@posteo.net>
2022-03-24 16:00:16 +0000

    [system] [emacs] Lots of updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
    set $fish_color_command cyan
    alias ls "lsd"
    alias cat "bat"
+   alias nix-fish "nix develop . --command \"fish\""
    starship init fish | source
 end
