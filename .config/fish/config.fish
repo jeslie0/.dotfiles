b71ad23fa884fbd03f5b7cb8fcc99157944fd45c
James Leslie <jamesleslie@posteo.net>
2022-06-22 22:21:52 +0100

    [winum] Merge all winum which keys
    Winum keys now all live in one place on which key, freeing up screen space.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+   set $fish_color_command cyan
+   alias ls "lsd"
+   alias cat "bat"
+   alias nix-fish "nix develop . --command \"fish\""
+   starship init fish | source
 end
